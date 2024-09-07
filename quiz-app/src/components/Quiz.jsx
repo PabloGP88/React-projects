7fbcd35dd5ed9ff0d527a760933b001465cad337
Jose Pablo Gonzalez  Poblette <pablogpp88@gmail.com>
2024-09-07 13:54:57 -0600

    basic logic for quiz app

---
+++ quiz-app/src/components/Quiz.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+import QUESTIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png';
+
+export default function Quiz()
+{
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const activeQuestionIndex = userAnswers.length;
+
+    const quizIsComplete = activeQuestionIndex == QUESTIONS.length;
+
+    if (quizIsComplete)
+        {
+            return <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy icon" />
+                <h2>Quiz completed!</h2>
+            </div>
+        } 
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
+    function handleSelectAnswer(selectedAnswer)
+    {
+        setUserAnswers((prevAnswers) => {
+            return [...prevAnswers,selectedAnswer]
+        });
+    }
+    
+    return(
+            <div id="quiz">
+                <div id="question">
+                    <h2>
+                        {QUESTIONS[activeQuestionIndex].text}
+                    </h2>
+                    <ul id="answers">
+                        {shuffledAnswers.map((answer) => (
+                            <li key={answer} className="answer">
+                                <button onClick={()=> handleSelectAnswer(answer)}>{answer}</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        );
+}
